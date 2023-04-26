3cff234a91436066a5c8180bb6ed29fb4f132d1e
fariya0 <120111405+fariya0@users.noreply.github.com>
2023-04-26 11:49:30 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders correctly", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttons = screen.getAllByRole("button");
+  expect(buttons).toHaveLength(2);
 });
